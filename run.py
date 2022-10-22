d60dfdc383dc600dc13ffa6b7833d6cd4cf99760
Christian Muise <christian.muise@gmail.com>
2022-10-22 00:21:56 -0400

    Initial edits.

--- run.py
+++ run.py
@@ -1,81 +1,99 @@
 
-from bauhaus import Encoding, proposition, constraint
+from bauhaus import Encoding, proposition, constraint, print_theory
 from bauhaus.utils import count_solutions, likelihood
 
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
+DICE = [1]#, 2, 3, 4]
+SIDE = [1, 2, 3, 4, 5, 6]
+COLOURS = ['red', 'green', 'blue', 'yellow']
+DIRECTION = ['top', 'bottom', 'left', 'right', 'front', 'back']
 
-    def __init__(self, data):
-        self.data = data
 
+PROPOSITIONS = []
+
+class Unique(object):
+    def __hash__(self):
+        return hash(str(self))
+    def __eq__(self, other):
+        return hash(self) == hash(other)
     def __repr__(self):
-        return f"A.{self.data}"
+        return str(self)
+    def __str__(self):
+        assert False, "You need to define the __str__ function on a proposition class"
 
 
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
+# Proposition for a dice side to have a colour
 @proposition(E)
-class FancyPropositions:
+class DiceSideCol(Unique):
+    def __init__(self, dice, side, colour):
+        self.dice = dice
+        self.side = side
+        self.colour = colour
 
-    def __init__(self, data):
-        self.data = data
+    def __str__(self):
+        return f"{self.dice}@{self.side}={self.colour}"
 
-    def __repr__(self):
-        return f"A.{self.data}"
+# Proposition for a dice side to be pointing in a particular orientation
+@proposition(E)
+class DiceSideDir(Unique):
+    def __init__(self, dice, side, direction):
+        self.dice = dice
+        self.side = side
+        self.direction = direction
 
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+    def __str__(self):
+        return f"{self.dice}-pointing-{self.side}={self.direction}"
 
+# Dice in a slot
+@proposition(E)
+class DiceInSlot(Unique):
+    def __init__(self, dice, slot):
+        self.dice = dice
+        self.slot = slot
 
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint((x & y).negate())
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
-
-    return E
+    def __str__(self):
+        return f"d{self.dice} in s{self.slot}"
 
 
-if __name__ == "__main__":
+###############
+# CONSTRAINTS #
+###############
 
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
-    # After compilation (and only after), you can check some of the properties
-    # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+# Each dice must have the side appear in exactly one direction
+for dice in DICE:
+    for side in SIDE:
+        constraint.add_exactly_one(E, [DiceSideDir(dice, side, direction) for direction in DIRECTION])
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+# Each direction must have exactly one side
+for dice in DICE:
+    for direction in DIRECTION:
+        constraint.add_exactly_one(E, [DiceSideDir(dice, side, direction) for side in SIDE])
+
+# Opposite sides must add up to 7
+for dice in DICE:
+    for side1 in SIDE:
+        for side2 in SIDE:
+            if side1 + side2 == 7:
+                E.add_constraint(DiceSideDir(dice, side1, 'top') >> DiceSideDir(dice, side2, 'bottom'))
+                E.add_constraint(DiceSideDir(dice, side1, 'bottom') >> DiceSideDir(dice, side2, 'top'))
+                E.add_constraint(DiceSideDir(dice, side1, 'left') >> DiceSideDir(dice, side2, 'right'))
+                E.add_constraint(DiceSideDir(dice, side1, 'right') >> DiceSideDir(dice, side2, 'left'))
+                E.add_constraint(DiceSideDir(dice, side1, 'front') >> DiceSideDir(dice, side2, 'back'))
+                E.add_constraint(DiceSideDir(dice, side1, 'back') >> DiceSideDir(dice, side2, 'front'))
+
+T = E.compile()
+E.introspect()
+
+sol = T.solve()
+E.pprint(T, sol)
+E.introspect(sol)
+print_theory(sol)
+
+# After compilation (and only after), you can check some of the properties
+# of your model:
+print("\nSatisfiable: %s" % T.satisfiable())
+print("# Solutions: %d" % count_solutions(T))
+print("   Solution: %s" % T.solve())
+
