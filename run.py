a6ee693392d78f730c557a050af122afcbdd20ee
Christian Muise <christian.muise@gmail.com>
2022-11-08 10:22:22 -0500

    Remaining draft of constraints and separate config file.

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 
 from bauhaus import Encoding, proposition, constraint, print_theory
 from bauhaus.utils import count_solutions, likelihood
+
+from nnf import config
+config.sat_backend = "kissat"
+
 from viz import viz_all_dice
+
+from diceconfigs import CONFIG as DICE_CONFIG
 
 # Encoding that will store all of your constraints
 E = Encoding()
 
 DICE = [1, 2, 3, 4]
+SLOTS = [1, 2, 3, 4]
 SIDE = [1, 2, 3, 4, 5, 6]
 COLOURS = ['red', 'green', 'blue', 'yellow']
 DIRECTION = ['top', 'bottom', 'left', 'right', 'front', 'back']
@@ -55,7 +62,17 @@ class DiceInSlot(Unique):
         self.slot = slot
 
     def __str__(self):
-        return f"d{self.dice} in slot-{self.slot}"
+        return f"slot(d{self.dice})={self.slot}"
+
+@proposition(E)
+class BoxColour(Unique):
+    def __init__(self, slot, direction, colour):
+        self.slot = slot
+        self.direction = direction
+        self.colour = colour
+
+    def __str__(self):
+        return f"box(slot-{self.slot} @ dir-{self.direction})={self.colour}"
 
 
 ###############
@@ -103,6 +120,50 @@ for dice in DICE:
         E.add_constraint((DiceSideDir(dice, 1, 'back') & DiceSideDir(dice, 2, cw_vertical[i])) >> DiceSideDir(dice, 3, cw_vertical[(i+3)%4]))
 
 
+# Specific dice configuration
+for dice in DICE_CONFIG:
+    for (d,s,c) in DICE_CONFIG[dice]:
+        E.add_constraint(DiceSideCol(d,s,c))
+
+
+####################
+# Slot constraints
+#####################
+
+# A dice can only be in one slot
+for dice in DICE:
+    constraint.add_exactly_one(E, [DiceInSlot(dice, slot) for slot in SLOTS])
+for slot in SLOTS:
+    constraint.add_exactly_one(E, [DiceInSlot(dice, slot) for dice in DICE])
+
+# If a dice of a particular configuration is in a slot, then the colour facing each direction is defined
+for dice in DICE:
+    for slot in SLOTS:
+        for col in COLOURS:
+            for direction in set(DIRECTION) - set(['left', 'right']):
+                E.add_constraint((DiceSideCol(dice, direction, col) & DiceSideDir(dice, side, direction) & DiceInSlot(dice, slot)) >> BoxColour(slot, direction, col))
+
+# A box side colour must have exactly one colour
+for slot in SLOTS:
+    for direction in DIRECTION:
+        constraint.add_exactly_one(E, [BoxColour(slot, direction, colour) for colour in COLOURS])
+
+
+#########################
+# Rules of the game
+#########################
+
+# For all of the sides (other than left and right), the box colours are unique
+for direction in set(DIRECTION) - set(['left', 'right']):
+    for s1 in SLOTS:
+        for s2 in SLOTS:
+            if s1 != s2:
+                for col in COLOURS:
+                    E.add_constraint(~BoxColour(s1, direction, col) | ~BoxColour(s2, direction, col))
+
+
+
+
 
 T = E.compile()
 # E.introspect()
@@ -117,6 +178,8 @@ sol = T.solve()
 print("\nSatisfiable: %s" % T.satisfiable())
 print("# Solutions: %d" % count_solutions(T))
 # print("   Solution: %s" % T.solve())
+
+
 print_theory(sol)
 print()
 print(viz_all_dice(sol, DICE))
